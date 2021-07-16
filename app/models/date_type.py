a236fc13594f0348fa9e30611adc8f6242786e0a
zachmyu <zach.m.yu@gmail.com>
2021-07-16 15:48:02 -0700

    Updated Model: Date Type

--- app/models/date_type.py
+++ app/models/date_type.py
@@ -7,24 +7,10 @@ class Date_type(db.Model, UserMixin):
     __tablename__ = 'date_types'
 
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(40), nullable=False, unique=True)
-    email = db.Column(db.String(255), nullable=False, unique=True)
-    hashed_password = db.Column(db.String(255), nullable=False)
-
-    @property
-    def password(self):
-        return self.hashed_password
-
-    @password.setter
-    def password(self, password):
-        self.hashed_password = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
+    type = db.Column(db.String(40), nullable=False, unique=True)
 
     def to_dict(self):
         return {
             'id': self.id,
-            'username': self.username,
-            'email': self.email
+            'type': self.type
         }
