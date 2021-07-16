2c0a68087b24f293f72710b703b59839c100abf6
zachmyu <zach.m.yu@gmail.com>
2021-07-16 15:51:43 -0700

    Updated Model: Favorite

--- app/models/favorite.py
+++ app/models/favorite.py
@@ -1,30 +1,16 @@
 from .db import db
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
-class User(db.Model, UserMixin):
-    __tablename__ = 'users'
+class Favorite(db.Model, UserMixin):
+    __tablename__ = 'favorites'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
-    email = db.Column(db.String(255), nullable=False, unique=True)
-    hashed_password = db.Column(db.String(255), nullable=False)
-
-    @property
-    def password(self):
-        return self.hashed_password
-
-    @password.setter
-    def password(self, password):
-        self.hashed_password = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
+    user_id = db.Column(db.Integer, nullable=False)
+    venue_id = db.Column(db.Integer, nullable=False)
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'username': self.username,
-            'email': self.email
+            'user_id': self.user_id,
+            'venue_id': self.venue_id
         }
