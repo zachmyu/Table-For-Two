e4c7220b7b129fd707a906a6d2a6ebd446df123c
zachmyu <zach.m.yu@gmail.com>
2021-07-16 20:25:21 -0700

    Updated Model: Review

--- app/models/review.py
+++ app/models/review.py
@@ -6,8 +6,10 @@ class Review(db.Model, UserMixin):
     __tablename__ = 'reviews'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, nullable=False, unique=True)
-    venue_id = db.Column(db.Integer, nullable=False, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'users.id'), nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey(
+        'venues.id'), nullable=False)
     title = db.Column(db.String(255), nullable=False)
     body = db.Column(db.Text(4000), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
