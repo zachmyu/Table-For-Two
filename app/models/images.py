172dec2eb758e1f10a48f4d08a4c203d73288b2e
Jefferson <jeffersongarcia1599@gmail.com>
2021-07-16 21:00:06 -0500

    Working on aws

--- app/models/images.py
+++ app/models/images.py
@@ -4,7 +4,7 @@ class Image(db.Model):
     __tablename__ = 'images'
     
     id = db.Column(db.Integer, primary_key=True)
-    photoUrl = db.Column(db.String(1000), nullable=False)
+    profileImg = db.Column(db.String(1000), nullable=False)
     userId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     
     user = db.relationship('User', back_populates='images')
@@ -12,6 +12,6 @@ class Image(db.Model):
     def to_dict(self):
         return {
             "id": self.id,
-            "photoUrl": self.photoUrl,
+            "profileImg": self.profileImg,
             "userId": self.userId
         }
