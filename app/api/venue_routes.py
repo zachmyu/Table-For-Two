bddec88b7c6bba77f5e2d69aae879f13d514d6a9
Jefferson <jeffersongarcia1599@gmail.com>
2021-07-19 01:47:41 -0500

    Working on venue_routes

--- app/api/venue_routes.py
+++ app/api/venue_routes.py
@@ -1,4 +1,18 @@
-# from flask import Blueprint, jsonify, request
-# from flask_login import login_required
+from flask import Blueprint, jsonify, request
+from flask_login import login_required
 
-# from app.models import User, db
+from app.models import Venue
+
+venue_routes = Blueprint('venues', __name__)
+
+
+@venue_routes.route('/')
+def venues():
+    venues = Venue.query.all()
+    return {"venues": [venue.to_dict()] for venue in venues}
+
+
+@venue_routes.route('/<int:id>')
+def venue(id):
+    venue = Venue.query.get(id)
+    return venue.to_dict()
---
+++ react-app/src/components/Venue/index.js
@@ -0,0 +1,8 @@
+import { useEffect } from "react";
+import { NavLink, useParams } from "react-router-dom";
+import Map from "../GoogleMap"
+
+
+function Venue() {
+    
+}
