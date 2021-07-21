ad9a2dca5fc2d557be8dad73d9420170b72ca6df
zachmyu <zach.m.yu@gmail.com>
2021-07-21 14:59:20 -0700

    Added login modal, demo button on login

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -7,6 +7,7 @@ import "./NavBar.css"
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import CalendarTodayRoundedIcon from '@material-ui/icons/CalendarTodayRounded';
 import DemoUser from '../DemoUser'
+import LoginFormModal from '../LoginFormModal';
 import { Button } from "@material-ui/core"
 import User from "../User"
 // import * as SessionActions from './SessionActions'
@@ -28,62 +29,51 @@ const NavBar = ({ loaded }) => {
   if (sessionUser) {
     sessionLinks = (
       <>
-        <li className='navbar-button'>
+        <div className='navbar-button'>
           <NavLink to='/users/:userId' exact={true} activeClassName='active'>
-            <AccountCircleIcon color= 'primary'></AccountCircleIcon>
+            <AccountCircleIcon color='primary'></AccountCircleIcon>
           </NavLink>
-        </li>
-        <li className='navbar-button'>
+        </div>
+        <div className='navbar-button'>
           <NavLink to='/users' exact={true} activeClassName='active'>
             <CalendarTodayRoundedIcon></CalendarTodayRoundedIcon>
           </NavLink>
-        </li>
-        <li className='navbar-button'>
+        </div>
+        <div className='navbar-button'>
           <LogoutButton />
           {/* <Button color= 'primary' variant='contained'> Poooop </Button> */}
-        </li>
+        </div>
       </>
     );
   } else {
     sessionLinks = (
       <>
-        <li className='navbar-button'>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li className='navbar-button'>
+        <div className='navbar-button'>
+          <LoginFormModal />
+        </div>
+        <div className='navbar-button'>
           <NavLink to='/sign-up' exact={true} activeClassName='active'>
             Sign Up
           </NavLink>
-        </li>
-        <li className='navbar-button'>
+        </div>
+        <div className='navbar-button'>
           <NavLink to='/' exact={true} activeClassName='active'>
-            <DemoUser></DemoUser>
+            <DemoUser />
           </NavLink>
-        </li>
+        </div>
       </>
     );
   }
 
   return (
-    <nav className='navbar__container'>
-      <ul>
-        <NavLink className='navbar__logo' exact to="/">
-          <img src='/logo.png' className="homepageLogo" alt="homepageLogo"></img>
-        </NavLink>
-        <div className='home-container'>
-          <li className='navbar-button'>
-            {/* <NavLink to='/' exact={true} activeClassName='active'>
-              TableForTwo
-            </NavLink> */}
-          </li>
-        </div>
-        <div className='session-container' >
-          {loaded && sessionLinks}
-        </div>
-      </ul>
-    </nav>
+    <div className='navbar__container'>
+      <NavLink className='navbar__logo' exact to="/">
+        <img src='/logo.png' className="homepageLogo" alt="homepageLogo"></img>
+      </NavLink>
+      <div className='session-container' >
+        {loaded && sessionLinks}
+      </div>
+    </div>
   );
 }
 
