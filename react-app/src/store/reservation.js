976443df4ed3be3c0130f6d550ee4349afe3c1b5
zachmyu <zach.m.yu@gmail.com>
2021-08-25 00:35:48 -0700

    Updated reservation thunks

---
+++ react-app/src/store/reservation.js
@@ -0,0 +1,142 @@
+const GET_SINGLE_RESERVATION = 'reservations/GET_SINGLE_RESERVATION'
+const READ_ALL_RESERVATIONS = 'reservations/READ_ALL_RESERVATIONS'
+const CREATE_RESERVATION = 'reservations/CREATE_RESERVATION'
+const UPDATE_RESERVATION = 'reservations/UPDATE_RESERVATION'
+const DELETE_RESERVATION = 'reservations/DELETE_RESERVATION'
+
+
+const loadSingleReservation = reservation => ({
+    type: GET_SINGLE_RESERVATION,
+    payload: reservation
+});
+
+const loadAllReservations = reservations => ({
+    type: READ_ALL_RESERVATIONS,
+    payload: reservations
+});
+
+const addReservation = reservation => ({
+    type: CREATE_RESERVATION,
+    payload: reservation
+});
+
+const updateReservation = reservation => ({
+    type: UPDATE_RESERVATION,
+    payload: reservation
+});
+
+const deleteReservation = reservation => ({
+    type: DELETE_RESERVATION,
+    payload: reservation
+});
+
+
+export const getOneReservation = reservationId => async dispatch => {
+    const res = await fetch(`/api/reservations/${reservationId}/`)
+    const data = await res.json();
+
+    if (res.ok) {
+        dispatch(loadSingleReservation(data))
+    }
+}
+
+export const getAllUserReservations = userId => async dispatch => {
+    const res = await fetch(`/api/reservations/user/${userId}/`);
+    const data = await res.json();
+
+    if (res.ok) {
+        dispatch(loadAllReservations(data))
+    }
+}
+
+export const createReservation = reservationData => async dispatch => {
+    const { user_id, venue_id, reservation_datetime, party_size, duration } = reservationData
+
+    const res = await fetch(`/api/reservations/create/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            user_id: user_id,
+            venue_id: venue_id,
+            reservation_datetime: reservation_datetime,
+            party_size: party_size,
+            duration: duration,
+        }),
+    });
+    const data = await res.json();
+
+    if (res.ok) {
+        dispatch(addReservation(data.menu_item))
+    }
+    return data
+}
+
+export const updateReservation = reservationData => async dispatch => {
+    const { user_id, venue_id, reservation_datetime, party_size, duration, reservationId } = reservationData
+    const res = await fetch(`/api/reservations/${reservationId}/`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            user_id: user_id,
+            venue_id: venue_id,
+            reservation_datetime: reservation_datetime,
+            party_size: party_size,
+            duration: duration,
+        }),
+    });
+    const data = await res.json();
+
+    if (res.ok) {
+        dispatch(updateReservation(data));
+    }
+    return data
+}
+
+export const deleteReservation = reservationId => async dispatch => {
+    const res = await fetch(`/api/reservations/${reservationId}/`, {
+        method: 'DELETE',
+    })
+
+    if (res.ok) {
+        dispatch(deleteReservation(reservationId));
+    }
+}
+
+
+const initialState = {}
+const menu_itemsReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case GET_SINGLE_RESERVATION:
+            newState = { ...state };
+            newState.current = action.payload;
+            return newState;
+
+        case READ_ALL_RESERVATIONS:
+            newState = { ...action.payload };
+            return newState
+
+        case CREATE_RESERVATION:
+            newState = Object.assign({}, state);
+            newState[action.payload.id] = action.payload;
+            return newState
+
+        case UPDATE_RESERVATION:
+            newState = { ...state };
+            newState.current = action.payload;
+            return newState;
+
+        case DELETE_RESERVATION:
+            newState = { ...state }
+            delete newState[action.payload]
+            return newState
+        default:
+            return state;
+    }
+}
+
+export default menu_itemsReducer;
