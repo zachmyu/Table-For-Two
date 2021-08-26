f6b4259beb6abc2dab127dbb98080b7b14e78720
zachmyu <zach.m.yu@gmail.com>
2021-08-25 19:33:26 -0700

    Updated Session thunks

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -2,7 +2,7 @@
 const SET_USER = 'session/SET_USER';
 const REMOVE_USER = 'session/REMOVE_USER';
 
-const setUser = (user) => ({
+const setUser = user => ({
 	type: SET_USER,
 	payload: user
 });
@@ -13,8 +13,8 @@ const removeUser = () => ({
 
 const initialState = { user: null };
 
-export const authenticate = () => async (dispatch) => {
-	const response = await fetch('/api/auth/', {
+export const authenticate = () => async dispatch => {
+	const response = await fetch(`/api/auth/`, {
 		headers: {
 			'Content-Type': 'application/json'
 		}
@@ -29,8 +29,8 @@ export const authenticate = () => async (dispatch) => {
 	}
 }
 
-export const login = (email, password) => async (dispatch) => {
-	const response = await fetch('/api/auth/login/', {
+export const login = (email, password) => async dispatch => {
+	const response = await fetch(`/api/auth/login/`, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
@@ -57,8 +57,8 @@ export const login = (email, password) => async (dispatch) => {
 
 }
 
-export const logout = () => async (dispatch) => {
-	const response = await fetch('/api/auth/logout/', {
+export const logout = () => async dispatch => {
+	const response = await fetch(`/api/auth/logout/`, {
 		headers: {
 			'Content-Type': 'application/json',
 		}
@@ -69,52 +69,9 @@ export const logout = () => async (dispatch) => {
 	}
 };
 
-
-
-export const signUp = (first_name, last_name, username, email, password, profile_image_url) => async (dispatch) => {
-	const formData = new FormData();
-
-	formData.append('first_name', first_name);
-	formData.append('last_name', last_name);
-	formData.append('username', username);
-	formData.append('email', email);
-	formData.append('password', password);
-
-	if (profile_image_url) formData.append("image", profile_image_url);
-
-	const response = await fetch('/api/auth/signup/', {
-		method: 'POST',
-		headers: {
-			'enctype': 'multipart/form-data',
-		},
-		body: formData,
-	})
-
-	const data = await response.json();
-	if (data.errors) {
-		return data
-	}
-	dispatch(setUser(data))
-	return data
-}
-
-//   if (response.ok) {
-//     const data = await response.json();
-//     dispatch(setUser(data))
-//     return null;
-//   } else if (response.status < 500) {
-//     const data = await response.json();
-//     if (data.errors) {
-//       return data.errors;
-//     }
-//   } else {
-//     return ['An error occurred. Please try again.']
-//   }
-// }
-
-export const signUp = (userData) => async (dispatch) => {
-	const { firstName, lastName, username, email, password, profile_image_url } = userData
-	const response = await fetch('/api/auth/signup/', {
+export const signUp = userData => async dispatch => {
+	const { firstName, lastName, username, email, password, profileImgUrl } = userData
+	const response = await fetch(`/api/auth/signup/`, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
@@ -122,12 +79,10 @@ export const signUp = (userData) => async (dispatch) => {
 		body: JSON.stringify({
 			first_name: firstName,
 			last_name: lastName,
-			username,
-			email,
-			about,
-			profile_pix: profPixUrl,
-			account_type: accountType,
-			password
+			username: username,
+			email: email,
+			profile_image_url: profileImgUrl,
+			password: password
 		}),
 	});
 
